2b2fa41837403fbd00bc6fa79286e5a57a3f7b2e
r.w.w.brouwer <r.w.w.brouwer@gmail.com>
2012-04-10 08:35:28 +0000

    Fixed the BWA Paired End mode

--- narwhal/tools/cleaner.sh
+++ narwhal/tools/cleaner.sh
@@ -13,8 +13,12 @@ find $INDIR -name '*.index' -exec readlink -f {} \; > files_index.txt
 find $INDIR -name '*.index.gz' -exec readlink -f {} \; >> files_index.txt
 
 #
-find $INDIR -name '*.fastq' -exec readlink -f {} \; > files_fastq.txt
-find $INDIR -name '*.fastq.gz' -exec readlink -f {} \; >> files_fastq.txt
+find $INDIR/raw_data -name '*.fastq' -exec readlink -f {} \; > files_raw_fastq.txt
+find $INDIR/raw_data -name '*.fastq.gz' -exec readlink -f {} \; >> files_raw_fastq.txt
+
+#
+find $INDIR/demultiplexed -name '*.fastq' -exec readlink -f {} \; > files_sample_fastq.txt
+find $INDIR/demultiplexed -name '*.fastq.gz' -exec readlink -f {} \; >> files_sample_fastq.txt
 
 #
 find $INDIR -name '*.srt.bam' -exec readlink -f {} \; > files_sortedbam.txt
